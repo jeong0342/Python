02f5e4f7b61dfe4f73a87efc65fb66680536195d
jeong0342 <64175895+jeong0342@users.noreply.github.com>
2020-05-12 19:09:39 +0900

    Update try01.py

--- try01.py
+++ try01.py
@@ -1,9 +1,25 @@
+# class Employee:
+#     def __init__ (self, name=0, dailywage=0, weekly=0):
+#         self.name = name
+#         self.salary = dailywage*weekly
+#     def calculateSalary()
+
+# # dailywage와weekly 곱 계산하여 출력
+# emp1 = Employee("Zara", 200, 5)
+# print(emp1.name, emp1.salary)
+
+#수정
 class Employee:
     def __init__ (self, name=0, dailywage=0, weekly=0):
         self.name = name
-        self.salary = dailywage*weekly
-    def calculateSalary()
+        self.dailywage = dailywage
+        self.weekly = weekly
+    def calculateSalary(self):
+        self.salary = self.dailywage*self.weekly
+        print(self.name,',', self.salary)
+
 
 # dailywage와weekly 곱 계산하여 출력
 emp1 = Employee("Zara", 200, 5)
+emp1.calculateSalary()
 print(emp1.name, emp1.salary)
